9bb809f572ea1ed579b1b821665a47ac4b71afac
Piyush Yadav <piyushyadavji930@gmail.com>
2026-09-24 01:24:09 +0530

    feat: add configuration layer

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env"
+    )
+    OPENAI_API_KEY: str
+    
+
+@lru_cache
+def get_settings()-> Settings:
+    return Settings()
