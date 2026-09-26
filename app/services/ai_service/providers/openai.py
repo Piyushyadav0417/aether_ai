88105ebb4adb1f3750bf19c3277cb97ccb1414c0
Piyush Yadav <piyushyadavji930@gmail.com>
2026-09-26 12:03:59 +0530

    added request and response sign in OpenAIProvider

--- app/services/ai_service/providers/openai.py
+++ app/services/ai_service/providers/openai.py
@@ -1,5 +1,6 @@
 from app.services.ai_service.base import AIRequest, AIResponse, AIProvider
 
 class OpenAIProvider(AIProvider):
-    async def get_response(self, request):
+    
+    async def get_response(self, request: AIRequest) -> AIResponse:
         pass
